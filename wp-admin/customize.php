f681ebc4618c56dfb0bab90ea8f1cc509fb84676
lethuanan <lethuanan859@gmail.com>
2021-10-06 09:06:57 +0700

    Initial Generated Site

---
+++ wp-admin/customize.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=https://lethuanan.github.io/MyWordPress/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%3A7000%2Fmyproject%2Fwordpress%2Fwp-admin%2Fcustomize.php&reauth=1">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "https://lethuanan.github.io/MyWordPress/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%3A7000%2Fmyproject%2Fwordpress%2Fwp-admin%2Fcustomize.php&reauth=1";
+		</script>
+
+		<p>You are being redirected to <a href="https://lethuanan.github.io/MyWordPress/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%3A7000%2Fmyproject%2Fwordpress%2Fwp-admin%2Fcustomize.php&reauth=1">https://lethuanan.github.io/MyWordPress/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%3A7000%2Fmyproject%2Fwordpress%2Fwp-admin%2Fcustomize.php&reauth=1</a></p>
+	</body>
+</html>
